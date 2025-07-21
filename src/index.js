32af440094848a45c946cbdad6cab206cb59ef7a
4R1u <62248461+4R1u@users.noreply.github.com>
2025-07-21 12:08:38 +0400

    feat: separate tab loading into its own script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import { loadMain } from "./load.js";
+
+loadMain();
---
+++ src/load.js
@@ -0,0 +1,17 @@
+function loadMain() {
+    const mainDiv = document.querySelector('#content');
+    const heading = document.createElement('h1');
+    const imagePlaceholder = document.createElement('div');
+    const loremIpsum = document.createElement('p');
+
+    heading.textContent = "Sample Restaurant";
+    imagePlaceholder.classList.add("image-placeholder");
+    imagePlaceholder.textContent = "Placeholder for an image";
+    loremIpsum.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis mollitia similique neque aliquid reprehenderit ipsam ducimus rerum dolorem! Totam, culpa. Voluptatum reiciendis minima quae veniam et. Voluptatem assumenda rerum aliquam?";
+
+    mainDiv.appendChild(heading);
+    mainDiv.appendChild(imagePlaceholder);
+    mainDiv.appendChild(loremIpsum);
+}
+
+export { loadMain };
